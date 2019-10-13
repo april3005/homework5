6b5c83ebcc990e31eff002c54c6cf6f238b3bb5e
april3005 <55992572+april3005@users.noreply.github.com>
2019-10-13 16:50:29 +0600

    Update MagicDoor.java

--- src/com/company/MagicDoor.java
+++ src/com/company/MagicDoor.java
@@ -2,19 +2,15 @@ package com.company;
 
 public class MagicDoor {
 
-    public static String heroes (){
-        String[] heroes = new String [4];
-        heroes [0] = "Warrior";
-        heroes [1] = "Wizard";
-        heroes [2] = "Doctor";
-        heroes [3] = "Kinetic";
+       public Hero[] heroes (){
+        Hero[] heroes = {"Warrior", "Wizard","Doctor", "Kinetic"};
 
         Hero Warrior = new Hero(250, 200, "Physical");
         Hero Wizard = new Hero (250, 200, "Magic");
         Hero Doctor = new Hero (250, 0, "Medical");
         Hero Kinetic = new Hero (250, 200, "Kinetic");
 
-        return heroes();
+        return heroes;
 
 
     }
